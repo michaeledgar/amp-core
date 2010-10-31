2e2b03cc2d298b5f4b8f070a4fc405f4a49d8b13
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-10-30 20:24:53 -0400

    Fixed the lack of Core namespace on autoload instructions.

--- lib/amp-core.rb
+++ lib/amp-core.rb
@@ -15,5 +15,22 @@ class Amp::Plugins::Core < Amp::Plugins::Base
     Amp::Support.class_eval do
       autoload :Template,                  "amp-core/templates/template.rb"
     end
+    Amp.class_eval do
+      Core.class_eval do
+        repositories = Module.new do
+          autoload :GenericRepoPicker,         "amp-core/repository/generic_repo_picker.rb"
+          autoload :AbstractLocalRepository,   "amp-core/repository/abstract/abstract_local_repo.rb"
+          autoload :AbstractStagingArea,       "amp-core/repository/abstract/abstract_staging_area.rb"
+          autoload :AbstractChangeset,         "amp-core/repository/abstract/abstract_changeset.rb"
+          autoload :AbstractVersionedFile,     "amp-core/repository/abstract/abstract_versioned_file.rb"
+          autoload :CommonChangesetMethods,    "amp-core/repository/abstract/common_methods/changeset.rb"
+          autoload :CommonLocalRepoMethods,    "amp-core/repository/abstract/common_methods/local_repo.rb"
+          autoload :CommonStagingAreaMethods,  "amp-core/repository/abstract/common_methods/staging_area.rb"
+          autoload :CommonChangesetMethods,    "amp-core/repository/abstract/common_methods/changeset.rb"
+          autoload :CommonVersionedFileMethods,"amp-core/repository/abstract/common_methods/versioned_file.rb"
+        end
+        const_set(:Repositories, repositories)
+      end
+    end
   end
 end
