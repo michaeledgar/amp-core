2833ba8c95bb37ece4b135fb8797f39088e55438
Michael Edgar <michael.j.edgar@dartmouth.edu>
2010-11-02 06:15:43 -0400

    Added rooted_file.rb, the old Opener class renamed to RootedOpener.

--- lib/amp-core.rb
+++ lib/amp-core.rb
@@ -11,17 +11,19 @@ class Amp::Plugins::Core < Amp::Plugins::Base
     module ::Amp
       module Core
         module Repositories
-          autoload :GenericRepoPicker,         "amp-core/repository/generic_repo_picker.rb"
-          autoload :AbstractLocalRepository,   "amp-core/repository/abstract/abstract_local_repo.rb"
-          autoload :AbstractStagingArea,       "amp-core/repository/abstract/abstract_staging_area.rb"
-          autoload :AbstractChangeset,         "amp-core/repository/abstract/abstract_changeset.rb"
-          autoload :AbstractVersionedFile,     "amp-core/repository/abstract/abstract_versioned_file.rb"
-          autoload :CommonChangesetMethods,    "amp-core/repository/abstract/common_methods/changeset.rb"
-          autoload :CommonLocalRepoMethods,    "amp-core/repository/abstract/common_methods/local_repo.rb"
-          autoload :CommonStagingAreaMethods,  "amp-core/repository/abstract/common_methods/staging_area.rb"
-          autoload :CommonChangesetMethods,    "amp-core/repository/abstract/common_methods/changeset.rb"
-          autoload :CommonVersionedFileMethods,"amp-core/repository/abstract/common_methods/versioned_file.rb"
+          autoload :GenericRepoPicker,         'amp-core/repository/generic_repo_picker.rb'
+          autoload :AbstractLocalRepository,   'amp-core/repository/abstract/abstract_local_repo.rb'
+          autoload :AbstractStagingArea,       'amp-core/repository/abstract/abstract_staging_area.rb'
+          autoload :AbstractChangeset,         'amp-core/repository/abstract/abstract_changeset.rb'
+          autoload :AbstractVersionedFile,     'amp-core/repository/abstract/abstract_versioned_file.rb'
+          autoload :CommonChangesetMethods,    'amp-core/repository/abstract/common_methods/changeset.rb'
+          autoload :CommonLocalRepoMethods,    'amp-core/repository/abstract/common_methods/local_repo.rb'
+          autoload :CommonStagingAreaMethods,  'amp-core/repository/abstract/common_methods/staging_area.rb'
+          autoload :CommonChangesetMethods,    'amp-core/repository/abstract/common_methods/changeset.rb'
+          autoload :CommonVersionedFileMethods,'amp-core/repository/abstract/common_methods/versioned_file.rb'
         end
+        module Support
+          autoload :RootedOpener, 'amp-core/support/rooted_opener.rb'
       end
     end
   }
