fca3186c9d5b5b06f2668f203df071312fb1e43d
Justin Love <git@JustinLove.name>
2010-11-07 21:36:41 -0600

    refactor plugin loading to ordinary require

--- lib/amp-core.rb
+++ lib/amp-core.rb
@@ -6,48 +6,11 @@ module Amp
 end
 
 class Amp::Plugins::Core < Amp::Plugins::Base
-  # this is necessary to prevent loading when #load! isn't called.
-  @loader = lambda {
-    $:.unshift(File.expand_path(File.dirname(__FILE__)))
-    module ::Amp
-      module Core
-        module Repositories
-          autoload :GenericRepoPicker,         'amp-core/repository/generic_repo_picker.rb'
-          autoload :AbstractLocalRepository,   'amp-core/repository/abstract/abstract_local_repo.rb'
-          autoload :AbstractStagingArea,       'amp-core/repository/abstract/abstract_staging_area.rb'
-          autoload :AbstractChangeset,         'amp-core/repository/abstract/abstract_changeset.rb'
-          autoload :AbstractVersionedFile,     'amp-core/repository/abstract/abstract_versioned_file.rb'
-          autoload :CommonChangesetMethods,    'amp-core/repository/abstract/common_methods/changeset.rb'
-          autoload :CommonLocalRepoMethods,    'amp-core/repository/abstract/common_methods/local_repo.rb'
-          autoload :CommonStagingAreaMethods,  'amp-core/repository/abstract/common_methods/staging_area.rb'
-          autoload :CommonChangesetMethods,    'amp-core/repository/abstract/common_methods/changeset.rb'
-          autoload :CommonVersionedFileMethods,'amp-core/repository/abstract/common_methods/versioned_file.rb'
-        end
-        module Support
-          autoload :EncodingUtils,  'amp-core/support/encoding_utils.rb'
-          autoload :Platform,       'amp-core/support/platform_utils.rb'
-          autoload :RootedOpener,   'amp-core/support/rooted_opener.rb'
-          autoload :StringUtils,    'amp-core/support/string_utils.rb'
-        end
-      end
-    end
-  }
-  class << self
-    attr_reader :loader
-  end
-
   def initialize(opts={})
     @opts = opts
   end
   
   def load!
-    puts "Loading amp-core..."
-    require 'amp-core/command_ext/repository_loading'
-    require 'amp-core/repository/repository.rb'
-    require 'amp-core/repository/generic_repo_picker.rb'
-    ::Amp::Support.class_eval do
-      autoload :Template,                  "amp-core/templates/template.rb"
-    end
-    self.class.loader.call
+    require 'amp-core/on_load'
   end
 end
